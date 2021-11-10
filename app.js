79c051d59163a3242edddabe3c6baba8de062082
Ian Christiansen <ianc.songdog@gmail.com>
2021-11-10 12:28:25 -0800

    grrr

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-// import functions and grab DOM elements
+import { survivorPerks } from './data/survivor-perks.js';
+// import { generatePerks } from './utils.js';
 
-// initialize global state
+const rollBtn = document.getElementById('roll-btn');
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+const generatePerks = () =>{
+    let randPerk1 = Math.floor(Math.random() * survivorPerks.length);
+    let randPerk2 = Math.floor(Math.random() * survivorPerks.length);
+    let randPerk3 = Math.floor(Math.random() * survivorPerks.length);
+    let randPerk4 = Math.floor(Math.random() * survivorPerks.length);
+
+    while (
+        randPerk1 === randPerk2 ||
+        randPerk1 === randPerk3 ||
+        randPerk1 === randPerk4 ||
+        randPerk2 === randPerk3 ||
+        randPerk2 === randPerk4 ||
+        randPerk3 === randPerk4 
+    )
+    {
+        randPerk1 = Math.floor(Math.random() * survivorPerks.length);
+        randPerk2 = Math.floor(Math.random() * survivorPerks.length);
+        randPerk3 = Math.floor(Math.random() * survivorPerks.length);
+        randPerk4 = Math.floor(Math.random() * survivorPerks.length);   
+    }
+};
+
+
+rollBtn.addEventListener('click', ()=> {
+    generatePerks;
+    console.log(generatePerks);
+});
