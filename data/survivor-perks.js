37cf8a326ee1779104f9977ef209bc5c31a291ba
Ian Christiansen <ianc.songdog@gmail.com>
2021-11-11 14:27:21 -0800

    added perk categories

--- data/survivor-perks.js
+++ data/survivor-perks.js
@@ -3,606 +3,710 @@ export const survivorPerks = [
         id: 1,
         name: 'Ace in the Hole',
         image: '../assets/aceinthehole.png',
-        category: 'resource',
+        category: 'Utility',
+        secondary: 'Loot'
     },
     {
         id: 2,
         name: 'Adrenaline',
         image: '../assets/adrenaline.png',
-        category: 'exhaustion',
+        category: 'Loop',
+        secondary: 'Exhaustion'
     },
     {
         id: 3,
         name: 'Aftercare',
         image: '../assets/aftercare.png',
-        category: 'healing',
+        category: 'Info',
+        secondary: 'Healing'
     },
     {
         id: 4,
         name: 'Alert',
         image: '../assets/alert.png',
-        category: 'info',
+        category: 'Info',
+        secondary: 'none'
     },
     {
         id: 5,
         name: 'Any Means Necessary',
         image: '../assets/anyMeansNecessary.png',
-        category: 'resource',
+        category: 'Utility',
+        secondary: 'none'
     },
     {
         id: 6,
         name: 'Appraisal',
         image: '../assets/appraisal.png',
-        category: 'resource',
+        category: 'Utility',
+        secondary: 'Loot'
     },
     {
         id: 7,
         name: 'Autodidact',
         image: '../assets/autodidact.png',
-        category: 'healing',
+        category: 'Utility',
+        secondary: 'Healing'
     },
     {
         id: 8,
         name: 'Balanced Landing',
         image: '../assets/balancedLanding.png',
-        category: 'exhaustion',
+        category: 'Loop',
+        secondary: 'Exhaustion'
     },
     {
         id: 9,
         name: 'Bite the Bullet',
         image: '../assets/biteTheBullet.png',
-        category: 'healing',
+        category: 'Stealth',
+        secondary: 'Healing'
     },
     {
         id: 10,
         name: 'Blast Mine',
         image: '../assets/blastMine.png',
-        category: 'info',
+        category: 'Utility',
+        secondary: 'none'
     },
     {
         id: 11,
         name: 'Blood Pact',
         image: '../assets/bloodPact.png',
-        category: 'healing',
+        category: 'Utility',
+        secondary: 'Healing'
     },
     {
         id: 12,
         name: 'Boil Over',
         image: '../assets/boilOver.png',
-        category: 'info',
+        category: 'Utility',
+        secondary: 'none'
     },
     {
         id: 13,
         name: 'Bond',
         image: '../assets/bond.png',
-        category: 'info',
+        category: 'Info',
+        secondary: 'none'
     },
     {
         id: 14,
         name: `Boon: Circle of Healing`,
         image: '../assets/boonCircleOfHealing.png',
-        category: 'healing',
+        category: 'Utility',
+        secondary: 'Healing',
+        Boon: 'Boon'
     },
     {
         id: 15,
         name: `Boon: Exponential`,
         image: '../assets/boonExponential.png',
-        category: 'healing',
+        category: 'Utility',
+        secondary: 'Healing',
+        Boon: 'Boon'
     },
     {
         id: 16,
         name: `Boon: Shadow Step`,
         image: '../assets/boonShadowStep.png',
-        category: 'stealth',
+        category: 'Stealth',
+        secondary: 'Loop',
+        Boon: 'Boon'
     },
     {
         id: 17,
         name: 'Borrowed Time',
         image: '../assets/borrowedTime.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'Second Chance'
     },
     {
         id: 18,
         name: 'Botany Knowledge',
         image: '../assets/botanyKnowledge.png',
-        category: 'healing',
+        category: 'Utility',
+        secondary: 'Healing'
     },
     {
         id: 19,
         name: 'Breakdown',
         image: '../assets/breakdown.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'none'
     },
     {
         id: 20,
         name: 'Breakout',
         image: '../assets/breakout.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'none'
     },
     {
         id: 21,
         name: 'Buckle Up',
         image: '../assets/buckleUp.png',
-        category: 'healing',
+        category: 'Utility',
+        secondary: 'Healing'
     },
     {
         id: 22,
         name: 'Built to Last',
         image: '../assets/builtToLast.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'Loot'
     },
     {
         id: 23,
         name: 'Calm Spirit',
         image: '../assets/calmSpirit.png',
-        category: 'utility',
+        category: 'Stealth',
+        secondary: 'none'
     },
     {
         id: 24,
         name: 'Clairvoyance',
         image: '../assets/clairvoyance.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'none'
     },
     {
         id: 25,
         name: 'Corrective Action',
         image: '../assets/correctiveAction.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'Stealth'
     },
     {
         id: 26,
         name: 'Counterforce',
         image: '../assets/counterforce.png',
-        category: 'utility',
+        category: 'Info',
+        secondary: 'none'
     },
     {
         id: 27,
         name: 'Dance With Me',
         image: '../assets/danceWithMe.png',
-        category: 'utility',
+        category: 'Stealth',
+        secondary: 'none'
     },
     {
         id: 28,
         name: 'Dark Sense',
         image: '../assets/darkSense.png',
-        category: 'utility',
+        category: 'Info',
+        secondary: 'none'
     },
     {
         id: 29,
         name: 'Dead Hard',
         image: '../assets/deadHard.png',
-        category: 'utility',
+        category: 'Loop',
+        secondary: 'Exhaustion'
     },
     {
         id: 30,
         name: 'Deception',
         image: '../assets/deception.png',
-        category: 'utility',
+        category: 'Stealth',
+        secondary: 'none'
     },
     {
         id: 31,
         name: 'Decisive Strike',
         image: '../assets/decisiveStrike.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'Second Chance'
     },
     {
         id: 32,
-        name: 'Deja Vu',
+        name: `Déjà Vu`,
         image: '../assets/dejaVu.png',
-        category: 'utility',
+        category: 'Info',
+        secondary: 'none'
     },
     {
         id: 33,
         name: 'Deliverance',
         image: '../assets/deliverance.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'Second Chance'
     },
     {
         id: 34,
         name: 'Desperate Measures',
         image: '../assets/desperateMeasures.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'Healing'
     },
     {
         id: 35,
         name: `Detective's Hunch`,
         image: '../assets/detectivesHunch.png',
-        category: 'utility',
+        category: 'Info',
+        secondary: 'none'
     },
     {
         id: 36,
         name: 'Distortion',
         image: '../assets/distortion.png',
-        category: 'utility',
+        category: 'Stealth',
+        secondary: 'none'
     },
     {
         id: 37,
         name: 'Diversion',
         image: '../assets/breakout.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'Stealth'
     },
     {
         id: 38,
         name: 'Empathy',
         image: '../assets/empathy.png',
-        category: 'healing',
+        category: 'Info',
+        secondary: 'Healing'
     },
     {
         id: 39,
         name: 'Fast Track',
         image: '../assets/empathy.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'none'
     },
     {
         id: 40,
         name: 'Flash Bang',
         image: '../assets/flashBang.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'none'
     },
     {
         id: 41,
         name: 'Flip Flop',
         image: '../assets/flipFlop.png',
-        category: 'healing',
+        category: 'Utility',
+        secondary: 'Healing'
     },
     {
         id: 42,
         name: 'For The People',
         image: '../assets/forThePeople.png',
-        category: 'healing',
+        category: 'Utility',
+        secondary: 'Healing'
     },
     {
         id: 43,
         name: 'Guardian',
         image: '../assets/guardian.png',
-        category: 'utility',
+        category: 'Stealth',
+        secondary: 'none'
     },
     {
         id: 44,
         name: 'Head On',
         image: '../assets/headOn.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'Exhaustion'
     },
     {
         id: 45,
         name: 'Hope',
         image: '../assets/hope.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'none'
     },
     {
         id: 46,
-        name: 'inner Healing',
+        name: 'Inner Healing',
         image: '../assets/innerHealing.png',
-        category: 'healing',
+        category: 'Utility',
+        secondary: 'Healing'
     },
     {
         id: 47,
         name: 'Iron Will',
         image: '../assets/ironWill.png',
-        category: 'utility',
+        category: 'Stealth',
+        secondary: 'none'
     },
     {
         id: 48,
         name: 'Kindred',
         image: '../assets/kindred.png',
-        category: 'utility',
+        category: 'Info',
+        secondary: 'none'
     },
     {
         id: 49,
         name: 'Kinship',
         image: '../assets/kinship.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'none'
     },
     {
         id: 50,
         name: 'Leader',
         image: '../assets/leader.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'none'
     },
     {
         id: 51,
         name: 'Left Behind',
         image: '../assets/leftBehind.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'none'
     },
     {
         id: 52,
         name: 'Lightweight',
         image: '../assets/lightweight.png',
-        category: 'utility',
+        category: 'Stealth',
+        secondary: 'none'
     },
     {
         id: 53,
         name: 'Lithe',
         image: '../assets/lithe.png',
-        category: 'utility',
+        category: 'Loop',
+        secondary: 'Exhaustion'
     },
     {
         id: 54,
         name: 'Lucky Break',
         image: '../assets/luckyBreak.png',
-        category: 'utility',
+        category: 'Stealth',
+        secondary: 'none'
     },
     {
         id: 55,
         name: 'Mettle of Man',
         image: '../assets/mettleOfMan.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'Mettle of Man'
     },
     {
         id: 56,
         name: 'No Mither',
         image: '../assets/noMither.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'Healing'
     },
     {
         id: 57,
         name: 'No One Left Behind',
         image: '../assets/noOneLeftBehind.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'Healing'
     },
     {
         id: 58,
         name: 'Object of Obsession',
         image: '../assets/objectOfObsession.png',
-        category: 'utility',
+        category: 'Info',
+        secondary: 'none'
     },  
     {
         id: 59,
         name: 'Off the Record',
         image: '../assets/offTheRecord.png',
-        category: 'utility',
+        category: 'Stealth',
+        secondary: 'none'
     },
     {
         id: 60,
         name: 'Open-handed',
         image: '../assets/openHanded.png',
-        category: 'utility',
+        category: 'Info',
+        secondary: 'none'
     },
     {
         id: 61,
         name: 'Overcome',
         image: '../assets/overcome.png',
-        category: 'utility',
+        category: 'Loop',
+        secondary: 'Exhaustion'
     },
     {
         id: 62,
         name: 'Pharmacy',
         image: '../assets/pharmacy.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'Loot'
     },
     {
         id: 63,
         name: `Plunderer's Instinct`,
         image: '../assets/plunderersInstinct.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'Loot'
     },
     {
         id: 64,
         name: 'Poised',
         image: '../assets/poised.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'Healing'
     },
     {
         id: 65,
         name: 'Power Struggle',
         image: '../assets/powerStruggle.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'Second Chance'
     },
     {
         id: 66,
         name: 'Premonition',
         image: '../assets/premonition.png',
-        category: 'utility',
+        category: 'Info',
+        secondary: 'none'
     },
     {
         id: 67,
         name: 'Prove Thyself',
         image: '../assets/proveThyself.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'none'
     },
     {
         id: 68,
         name: 'Quick and Quiet',
         image: '../assets/quickAndQuiet.png',
-        category: 'utility',
+        category: 'Stealth',
+        secondary: 'none'
     },
     {
         id: 69,
         name: 'Red Herring',
         image: '../assets/redHerring.png',
-        category: 'utility',
+        category: 'Stealth',
+        secondary: 'none'
     },
     {
         id: 70,
         name: 'Renewal',
         image: '../assets/renewal.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'Healing'
     },
     {
         id: 71,
         name: 'Repressed Alliance',
         image: '../assets/repressedAlliance.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'none'
     },
     {
         id: 72,
         name: 'Resilience',
         image: '../assets/resilience.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'none'
     },
     {
         id: 73,
         name: 'Resurgence',
         image: '../assets/resurgence.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'Healing'
     },
     {
         id: 74,
         name: 'Rookie Spirit',
         image: '../assets/rookieSpirit.png',
-        category: 'utility',
+        category: 'Info',
+        secondary: 'none'
     },
     {
         id: 75,
         name: 'Saboteur',
         image: '../assets/saboteur.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'none'
     },
     {
         id: 76,
         name: 'Self Aware',
         image: '../assets/selfAware.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'none'
     },
     {
         id: 77,
         name: 'Self Care',
         image: '../assets/selfCare.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'Healing'
     },
     {
         id: 78,
         name: 'Self Preservation',
         image: '../assets/openHanded.png',
-        category: 'utility',
+        category: 'Stealth',
+        secondary: 'none'
     },
     {
         id: 79,
         name: 'Situational Awareness',
         image: '../assets/situationalAwareness.png',
-        category: 'utility',
+        category: 'Info',
+        secondary: 'none'
     },
     {
         id: 80,
         name: 'Slippery Meat',
         image: '../assets/slipperyMeat.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'Second Chance'
     },
     {
         id: 81,
         name: 'Small Game',
         image: '../assets/smallGame.png',
-        category: 'utility',
+        category: 'Info',
+        secondary: 'none'
     },
     {
         id: 82,
         name: 'Smash Hit',
         image: '../assets/smashHit.png',
-        category: 'utility',
+        category: 'Loop',
+        secondary: 'Exhaustion'
     },
     {
         id: 83,
-        name: 'soleSurvivor',
+        name: 'Sole Survivor',
         image: '../assets/soleSurvivor.png',
-        category: 'utility',
+        category: 'Stealth',
+        secondary: 'none'
     },
     {
         id: 84,
         name: 'Solidarity',
         image: '../assets/solidarity.png',
-        category: 'healing',
+        category: 'Utility',
+        secondary: 'Healing' 
     },
     {
         id: 85,
         name: 'Soul Guard',
         image: '../assets/soulGuard.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'Healing'
     },
     {
         id: 86,
         name: 'Spine Chill',
         image: '../assets/spineChill.png',
-        category: 'utility',
+        category: 'Info',
+        secondary: 'none'
     },
     {
         id: 87,
         name: 'Sprint Burst',
         image: '../assets/sprintBurst.png',
-        category: 'utility',
+        category: 'Loop',
+        secondary: 'Exhaustion'
     },
     {
         id: 88,
         name: 'Stake Out',
         image: '../assets/stakeOut.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'none'
     },
     {
         id: 89,
         name: 'Streetwise',
         image: '../assets/streetwise.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'Loot'
     },
     {
         id: 90,
         name: 'Technician',
         image: '../assets/technician.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'Loot'
     },
     {
         id: 91,
         name: 'Tenacity',
         image: '../assets/tenacity.png',
-        category: 'healing',
+        category: 'Utility',
+        secondary: 'Healing'
     },
     {
         id: 92,
         name: 'This is not Happening',
         image: '../assets/thisIsNotHappening.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'none'
     },
     {
         id: 93,
         name: 'Unbreakable',
         image: '../assets/unbreakable.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'Second Chance'
     },
     {
         id: 94,
         name: 'Up the Ante',
         image: '../assets/upTheAnte.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'none'
     },
     {
         id: 95,
         name: 'Urban Evasion',
         image: '../assets/urbanEvasion.png',
-        category: 'utility',
+        category: 'Stealth',
+        secondary: 'none'
     },
     {
         id: 96,
         name: 'Vigil',
         image: '../assets/vigil.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'none'
     },
     {
         id: 97,
         name: 'Visionary',
         image: '../assets/visionary.png',
-        category: 'utility',
+        category: 'Info',
+        secondary: 'none'
     },
     {
         id: 98,
         name: 'Wake Up',
         image: '../assets/wakeUp.png',
-        category: 'utility',
+        category: 'Info',
+        secondary: 'none'
     },
     {
         id: 99,
         name: `We'll Make it`,
         image: '../assets/wellMakeIt.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'Healing'
     },
     {
         id: 100,
         name: `We're Gonna Live Forever`,
         image: '../assets/wereGonnaLiveForever.png',
-        category: 'utility',
+        category: 'Utility',
+        secondary: 'Healing'
     },
     {
         id: 101,
         name: 'Windows of Opportunity',
-        image: '../assets/windowsofopportunity.png',
-        category: 'loop',
+        image: '../assets/windowsOfOpportunity.png',
+        category: 'Info',
+        secondary: 'none'
     },
 ];
