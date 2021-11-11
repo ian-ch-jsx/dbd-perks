01adf5d023bc57fc7085031e76b996e1b67a5246
Ian Christiansen <ianc.songdog@gmail.com>
2021-11-10 22:27:16 -0800

    perks randomly generating

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 import { survivorPerks } from './data/survivor-perks.js';
-// import { generatePerks } from './utils.js';
 
 const rollBtn = document.getElementById('roll-btn');
 
@@ -16,17 +15,24 @@ const generatePerks = () =>{
         randPerk2 === randPerk3 ||
         randPerk2 === randPerk4 ||
         randPerk3 === randPerk4 
-    )
-    {
+    ){
         randPerk1 = Math.floor(Math.random() * survivorPerks.length);
         randPerk2 = Math.floor(Math.random() * survivorPerks.length);
         randPerk3 = Math.floor(Math.random() * survivorPerks.length);
         randPerk4 = Math.floor(Math.random() * survivorPerks.length);   
     }
+    let survPerk1 = survivorPerks[randPerk1];
+    console.log(survPerk1.name);
+
+    let survPerk2 = survivorPerks[randPerk2];
+    console.log(survPerk2.name);
+
+    let survPerk3 = survivorPerks[randPerk3];
+    console.log(survPerk3.name);
+
+    let survPerk4 = survivorPerks[randPerk4];
+    console.log(survPerk4.name);
 };
-
-
+generatePerks();
 rollBtn.addEventListener('click', ()=> {
-    generatePerks;
-    console.log(generatePerks);
 });
